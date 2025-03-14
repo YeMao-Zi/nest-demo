48bf743a0356be36cb2ed6c82b73f16e63fc187b
zsj <1794541268@qq.com>
2025-03-14 11:18:17 +0800

    init

---
+++ src/person/person.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  Query,
+} from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+@Controller('person')
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  // @Inject(PersonService)
+  // private personService: PersonService;
+  @Post()
+  create(@Body() createPersonDto: CreatePersonDto) {
+    return this.personService.create(createPersonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.personService.findAll();
+  }
+
+  @Get('find')
+  query(@Query('name') name: String, @Query('age') age: Number) {
+    return `received： name: ${name}, age: ${age}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.personService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personService.update(+id, updatePersonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.personService.remove(+id);
+  }
+}
