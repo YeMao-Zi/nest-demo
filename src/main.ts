7c29e1b1f844359beca7c5d5ea188b8b97fa7720
zsj <1794541268@qq.com>
2025-05-08 15:33:32 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // 开启文件上传功能
+  // 把 public 目录设置为静态文件目录（http://localhost:3000/static/test.html可访问）
   app.useStaticAssets('public', { prefix: '/static' });
-
+  // 把 uploads 目录设置为静态文件目录（用于上传）
+  app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
